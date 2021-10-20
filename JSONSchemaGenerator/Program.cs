e9b1a294c7ea3802ce646a1a88f6c4954c967859
Richard Fox <richard.fox@ahref.co.uk>
2021-10-20 01:29:30 -0700

    Initial Files

---
+++ JSONSchemaGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using FrooxEngine;
+using NeosHeadless;
+using NJsonSchema.Generation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONSchemaGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new JsonSchemaGeneratorSettings();
+            var generator = new JsonSchemaGenerator(settings);
+
+            Type[] types = { typeof(WorldStartupParameters), typeof(WorldStartInfo), typeof(Config), typeof(NeosConfig) };
+
+            // This will get the current PROJECT directory
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            string outputPath = projectDirectory + "/output/";
+            Directory.CreateDirectory(outputPath);
+
+            foreach (Type type in types)
+            {
+                File.WriteAllText($"{outputPath}{type.Name}.json", generator.Generate(type).ToJson());
+            }
+        }
+    }
+}
